eeb74b8ee5acccd7e924c8da970188ba080a0e47
0xCornelius <0xCornelius@protonmail.com>
2022-04-09 10:54:25 +0300

    add NFTSVG libraries

---
+++ packages/hardhat/contracts/libraries/HabitStructs.sol
@@ -0,0 +1,38 @@
+pragma solidity 0.8.4;
+
+library HabitStructs {
+
+    struct Commitment {
+        uint256 timesPerTimeframe;
+        //Habit should be done every X time
+        uint256 timeframe;
+        //Amount of times the habit has to be done to allow the bid amount to be withdrawn
+        uint256 chainCommitment;
+    }
+
+    struct Accomplishment {
+        //Amount of times the habit has been accomplished
+        uint256 chain;
+        uint256 periodStart;
+        uint256 periodEnd;
+        uint256 periodTimesAccomplished;
+        string[] proofs;
+    }
+
+    struct HabitData {
+        uint256 id;
+        string name;
+        string description;
+        uint256 stake;
+        bool stakeClaimed;
+        address beneficiary;
+        Commitment commitment;
+        Accomplishment accomplishment;
+    }
+
+    struct HabitDataWithOwner {
+        HabitData habitData;
+        address owner;
+    }
+    
+}
